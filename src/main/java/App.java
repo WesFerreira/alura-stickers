29a6c55ff8b7e26cdb15eb7ba8e07927e25cb296
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-03-28 14:13:22 -0300

    HTTP conection and search top movies

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class App {
 
-    
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // fazer conexão HTTP e buscar os top filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        var endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // extrair só os dados que interessam (título, poster, classificação)
+
+        // exibir e manipular os dados
+
+    }
 
 }
